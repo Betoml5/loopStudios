cc3ee6dea843e0bcd12e717f188f7dbbf87e5bba
Beto <betoml5@hotmail.com>
2021-03-14 17:31:49 -0600

    Animations, opacity, pointers

--- src/components/Creation.jsx
+++ src/components/Creation.jsx
@@ -1,7 +1,7 @@
 const Creation = (props) => {
   console.log(props);
   return (
-    <div className="relative w-full">
+    <div className="relative w-full cursor-pointer hover:opacity-90 transition-all ease-out">
       <p className="text-2xl leading-6 text-white font-thin uppercase bottom-4 left-2 w-1/2 absolute">
         {props.data.title}
       </p>
